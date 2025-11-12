6503c67354e902af4ef7cdfdcff62b49757b3f8b
theBost-Guardia <mhenriot@guardiaschool.fr>
2025-11-12 16:20:44 +0100

    Vertion a montrer au prof

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// Attend que tout le HTML soit chargé avant d'exécuter le script
 document.addEventListener('DOMContentLoaded', () => {
-
-    // Sélectionne les deux éléments par leur ID
     const icone = document.getElementById('dossier-icone');
     const description = document.getElementById('description-cachee');
 
-    // Ajoute un "écouteur d'événement" pour le clic sur l'icône
     icone.addEventListener('click', () => {
-        
-        // Alterne la classe 'visible' sur le paragraphe
-        // Si le paragraphe l'a, il l'enlève.
-        // S'il ne l'a pas, il l'ajoute.
         description.classList.toggle('visible');
     });
-
 });
